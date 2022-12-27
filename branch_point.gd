adc514f30389556e843e11b49fa01d901779b88e
Antonio Dell'Annunziata <a.dellannunziata@einhundert-energie.de>
2022-12-27 16:12:39 +0100

    feat: Add first tree implementation

---
+++ branch_point.gd
@@ -0,0 +1,51 @@
+extends Node2D
+class_name BranchPoint
+
+const MAX_ROTATION_DEGREES := 45.0
+const MAX_BRANCHES := 3
+
+@export var grow_direction := Vector2.ZERO
+@export var max_length := 0
+
+var can_grow := true
+
+var _branches_count := 0
+
+func _ready():
+	if grow_direction == Vector2.ZERO:
+		push_error("BranchPoint %s added to tree without grow_direction set." % name)
+	elif max_length == 0:
+		push_error("BranchPoint %s added to tree without max_length set." % name)
+
+
+func grow() -> BranchPoint:
+	var direction := _get_grow_direction()
+	var length := _get_grow_length()
+	var branch_end := direction * length
+	
+	var branch_line = Line2D.new()
+	branch_line.add_point(Vector2.ZERO)
+	branch_line.add_point(branch_end)
+	add_child(branch_line)
+	
+	var new_branch_point = BranchPoint.new()
+	new_branch_point.position = branch_end
+	add_child(new_branch_point)
+	
+	_branches_count += 1
+	if _branches_count >= MAX_BRANCHES:
+		can_grow = false
+	
+	return new_branch_point
+
+
+func _get_grow_direction() -> Vector2:
+	var deviation = randf_range(-1, 1)
+	var angle = MAX_ROTATION_DEGREES * deviation
+	
+	return grow_direction.rotated(deg_to_rad(angle)).normalized()
+
+
+func _get_grow_length() -> float:
+	var deviation := randf_range(.75, 1)
+	return deviation * max_length
--- tree.gd
+++ tree.gd
@@ -2,22 +2,52 @@ extends Node2D
 
 
 const max_height = 10
-const grow_amount = -16
+const grow_amount = 16
 
 
 var _height = 1
-var _branch_points: Array[Node2D] = []
+var _branch_points: Array[BranchPoint] = []
 
 
-@onready var trunk := $Trunk
-@onready var branch_points := $BranchPoints
+@onready var _trunk := $Trunk
+@onready var _branches := $Branches
+
+
+func _ready():
+	var initial_point = BranchPoint.new()
+	initial_point.grow_direction = Vector2(-0.5, -1) if randi_range(0, 1) == 0 else Vector2(0.5, -1)
+	initial_point.max_length = grow_amount
+	initial_point.position.y -= grow_amount
+	_branch_points.append(initial_point)
+	_branches.add_child(initial_point)
+
 
 func _grow():
 	_height += 1
-	var next_trunk_point = trunk.get_point_position(trunk.get_point_count() - 1)
-	next_trunk_point.y += grow_amount
-	trunk.add_point(next_trunk_point)
-	branch_points
+	_branches.position.y -= grow_amount
+	
+	var next_trunk_point = _trunk.get_point_position(_trunk.get_point_count() - 1)
+	next_trunk_point.y -= grow_amount
+	_trunk.add_point(next_trunk_point)
+	
+	
+func _move_branche_points():
+	if _height >= .5 * max_height:
+		_branches.position.y -= grow_amount
+	else: 
+		for branch_point in _branch_points:
+			branch_point.position.y -= grow_amount
+
+func _get_growing_points() -> Array[BranchPoint]:
+	var point_count = ceili(_branch_points.size() / 2)
+	var selected_points: Array[BranchPoint] = []
+	var i = 0
+	while i < point_count:
+		var point = _branch_points.pick_random()
+		if not selected_points.has(point):
+			selected_points.append(point)
+			i += 1
+	return selected_points
 
 
 func _on_grow_timer_timeout():
